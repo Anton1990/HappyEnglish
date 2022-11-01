f1bec1569594c5cbe05a618e7e612bfb60e2f9f9
Anton Lukyantsev <Lukyantsev1990@yandex.ru>
2022-11-01 12:26:02 +0300

    RepositoryPattern

---
+++ HappyEnglishWebApi.Model/HappyEnglishWebApi.Model/HappyEnglishWebApi.Model/Gamer.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HappyEnglishWebApi.Model
+{
+    public class Gamer
+    {
+        public long Id { get; set; } = 11;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ HappyEnglishWebApi.Model/HappyEnglishWebApi.Model/HappyEnglishWebApi.Model/Word.cs
@@ -0,0 +1,8 @@
+﻿namespace HappyEnglishWebApi.Model
+{
+    public class Word
+    {
+        public long Id { get; set; } = 1;
+        public string Value { get; set; }
+    }
+}
